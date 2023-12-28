a617a0633def25996be5d16e836862285c965363
Akash Potti <akashpotti07@gmail.com>
2023-12-28 10:10:38 +0530

    initial commit

---
+++ bank.cpp
@@ -0,0 +1,194 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount
+{
+    char name[20];
+    int account_number;
+    float balance;
+
+public:
+    void get(int index);
+    void deposit();
+    void withdraw();
+    void display();
+    void check_balance();
+};
+
+void BankAccount::get(int index)
+{
+    account_number = index;
+    cout << "Account number assinged is:" << account_number << endl;
+    cout << "Enter the name of the account holder: ";
+    cin >> name;
+    cout << "Enter the balance: ";
+    cin >> balance;
+}
+
+void BankAccount::deposit()
+{
+    float amount;
+    cout << "Enter the amount to be deposited: ";
+    cin >> amount;
+    balance += amount;
+}
+
+void BankAccount::withdraw()
+{
+    float amount;
+    cout << "Enter the amount to be withdrawn: ";
+    cin >> amount;
+    if (amount > balance)
+    {
+        cout << "Insufficient balance" << endl;
+    }
+    else
+    {
+        balance -= amount;
+    }
+}
+
+void BankAccount::display()
+{
+    cout << "Name: " << name << endl;
+    cout << "Account number: " << account_number << endl;
+    cout << "Balance: " << balance << endl;
+}
+
+void BankAccount::check_balance()
+{
+    cout << "Balance: " << balance << endl;
+}
+
+int main()
+{
+    int size = 1;
+    BankAccount *accounts = new BankAccount[size];
+
+    int count = 0;
+
+    while (1)
+    {
+        cout << "1. Enter the details of the account holder" << endl;
+        cout << "2. Deposit money" << endl;
+        cout << "3. Withdraw money" << endl;
+        cout << "4. Display details" << endl;
+        cout << "5. Display balance" << endl;
+        cout << "6. Exit" << endl;
+        int choice;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            if (count < size)
+            {
+                accounts[count].get(count);
+                count++;
+            }
+            else
+            {
+                cout << "User note found creating new account" << endl;
+                size *= 2;
+                BankAccount *temp = new BankAccount[size];
+                for (int i = 0; i < count; ++i)
+                {
+                    temp[i] = accounts[i];
+                }
+                delete[] accounts;
+                accounts = temp;
+
+                accounts[count].get(count);
+                count++;
+            }
+            break;
+        case 2:
+
+            if (count > 0)
+            {
+                int accountIndex;
+                cout << "Enter the account number: ";
+                cin >> accountIndex;
+                if (accountIndex >= 0 && accountIndex < count)
+                {
+                    accounts[accountIndex].deposit();
+                }
+                else
+                {
+                    cout << "Invalid account index" << endl;
+                }
+            }
+            else
+            {
+                cout << "No accounts exist. Please create an account first." << endl;
+            }
+            break;
+        case 3:
+            if (count > 0)
+            {
+                int accountIndex;
+                cout << "Enter the account number: ";
+                cin >> accountIndex;
+                if (accountIndex >= 0 && accountIndex < count)
+                {
+                    accounts[accountIndex].withdraw();
+                }
+                else
+                {
+                    cout << "Invalid account index" << endl;
+                }
+            }
+            else
+            {
+                cout << "No accounts exist. Please create an account first." << endl;
+            }
+            break;
+        case 4:
+            if (count > 0)
+            {
+                int accountIndex;
+                cout << "Enter the account number: ";
+                cin >> accountIndex;
+                if (accountIndex >= 0 && accountIndex < count)
+                {
+                    accounts[accountIndex].display();
+                }
+                else
+                {
+                    cout << "Invalid account index" << endl;
+                }
+            }
+            else
+            {
+                cout << "No accounts exist. Please create an account first." << endl;
+            }
+            break;
+        case 5:
+            // Display balance for a specific account
+            if (count > 0)
+            {
+                int accountIndex;
+                cout << "Enter the account number: ";
+                cin >> accountIndex;
+                if (accountIndex >= 0 && accountIndex < count)
+                {
+                    accounts[accountIndex].check_balance();
+                }
+                else
+                {
+                    cout << "Invalid account index" << endl;
+                }
+            }
+            else
+            {
+                cout << "No accounts exist. Please create an account first." << endl;
+            }
+            break;
+        case 6:
+            exit(0);
+        default:
+            cout << "Invalid choice" << endl;
+        }
+    }
+}
---
+++ election.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+class election
+{
+    char name[20];
+    int vote;
+
+public:
+    void get();
+    float vote_percentage();
+    static election winner(election e[], int candidate);
+    void display();
+    static int total_votes;
+};
+
+int election::total_votes;
+void election::get()
+{
+    cout << "Enter the name of the candidate: ";
+    cin >> name;
+    cout << "Enter the number of votes: ";
+    cin >> vote;
+    total_votes += vote;
+}
+
+float election::vote_percentage()
+{
+    float percent;
+    percent = (float)vote / total_votes * 100;
+    return percent;
+}
+
+election election::winner(election e[], int candidate)
+{
+    int max = 0;
+    int windex = 0;
+
+    for (int i = 0; i < candidate; i++)
+    {
+        if (e[i].vote > max)
+        {
+            max = e[i].vote;
+            windex = i;
+        }
+    }
+    return e[windex];
+}
+
+void election::display()
+{
+    float percent = vote_percentage();
+    cout << "Candidate: " << name << endl;
+    cout << "Number of votes: " << vote << endl;
+    cout << "Percemtage of vote recieved is " << percent << endl;
+}
+
+int main() // Corrected the return type of main to int
+{
+    int candidate;
+    cout << "Enter the number of candidates: ";
+    cin >> candidate;
+
+    election *e = new election[candidate];
+
+    for (int i = 0; i < candidate; i++)
+    {
+        e[i].get();
+    }
+
+    for (int i = 0; i < candidate; i++)
+    {
+        e[i].display();
+    }
+
+    cout << "The winner is ";
+    election winner = election::winner(e, candidate);
+    winner.display();
+
+    delete[] e;
+
+    return 0;
+}
